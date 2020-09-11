f4f28d14da89e416c66598c62b634f852f723fb3
Maciej Kwaśniak <kmaciek17@gmail.com>
2020-09-11 11:04:30 +0200

    Update dummy

--- packages/dummy/src/index.tsx
+++ packages/dummy/src/index.tsx
@@ -1,78 +1,42 @@
 import { render } from "@vzn/dom";
-import { onCleanup, createState, useContext, createContext } from "@vzn/core";
+import { onCleanup, createState } from "@vzn/core";
+import { StoreRegistry, StoreRegistryContext, useStore } from "@vzn/store";
 
-const TestContext = createContext({ counter: 0 });
+const AuthStore = () => ({
+  test: 'Works'
+});
 
-// export class Registry {
-//   private registry = new Map();
+const CounterStore = () => ({
+  counter: 0,
+  authState: useStore(AuthStore),
 
-//   lookup<T>(key: any): T | undefined {
-//     return this.registry.get(key);
-//   }
-
-//   register<T>(key: any, value: T): T {
-//     this.registry.set(key, value);
-//     return value;
-//   }
-// }
-
-// const RegistryContext = createContext(new Registry());
-
-// export const useRegistry = () => useContext(RegistryContext)
-
-// function useService<T>(serviceConstructor: () => T): T {
-//   const registry = useRegistry();
-//   const service = registry.lookup<T>(serviceConstructor);
-//   return service || registry.register(serviceConstructor, serviceConstructor());
-// }
-
-// function createService<T extends object>(targetConstructor: () => T) {
-//   return () => createState(targetConstructor())
-// }
-
-// const AuthService = createService(() => ({
-//   test: 'Works'
-// }));
-
-// const CounterService = createService(() => ({
-//   counter: 0,
-//   authService: useService(AuthService),
-
-//   get auth() {
-//     return `${this.authService.test} - ${this.counter}`;
-//   }
-// }))
+  get auth() {
+    return `${this.authState.test} - ${this.counter}`;
+  }
+})
 
 function Clock() {
-  const context = useContext(TestContext);
+  const state = createState({
+    counter: useStore(CounterStore),
 
-  const timer = setInterval(() => context.counter++, 1000);
+    increment() {
+      this.counter.counter++
+    }
+  })
+  
+  const timer = setInterval(() => state.increment(), 1000);
   onCleanup(() => clearInterval(timer));
 
-  return <div>{context.counter}</div>;
+  return <div>{state.counter.auth}</div>;
 }
 
 function App() {
-  const state = createState({
-    show: true,
-
-    toggle() {
-      this.show = !this.show
-    }
-  })
-
-  const counter = createState({ counter: 0 });
+  const registry = new StoreRegistry()
 
   return (
-    <>
-      <button onClick={state.toggle}>Toggle</button>
-
-      {state.show ? (
-        <TestContext.Provider value={counter}>
-          <Clock />
-        </TestContext.Provider>
-      ) : ''}
-    </>
+    <StoreRegistryContext.Provider value={registry}>
+      <Clock />
+    </StoreRegistryContext.Provider>
   );
 }
 
