29b9a63e5888c200508067e9743947ff7a28c1fa
Maciej Kwaśniak <kmaciek17@gmail.com>
2020-09-13 13:00:35 +0200

    Remove support for class components

--- packages/@vzn/core/src/rendering/component.ts
+++ packages/@vzn/core/src/rendering/component.ts
@@ -1,42 +1,23 @@
 import { untracked } from "mobx";
 
-type PropsWithChildren<P = {}> = P & { children?: JSX.Element };
+export type PropsWithChildren<P> = P & { children?: JSX.Element };
 
-type ComponentConstructor<P> =
-  | FunctionComponent<P>
-  | (new (props: PropsWithChildren<P>) => JSX.Element);
+export type ComponentProps<T extends keyof JSX.IntrinsicElements | ComponentType<any>> =
+  T extends ComponentType<infer P>
+    ? P
+    : T extends keyof JSX.IntrinsicElements
+        ? JSX.IntrinsicElements[T]
+        : {};
 
-export type FunctionComponent<P = {}> = (props: PropsWithChildren<P>) => JSX.Element;
 
-export type ComponentProps<
-  T extends keyof JSX.IntrinsicElements | ComponentConstructor<any>
-> = T extends ComponentConstructor<infer P>
-  ? P
-  : T extends keyof JSX.IntrinsicElements
-  ? JSX.IntrinsicElements[T]
-  : {};
-
-export class Component<P extends PropsWithChildren = {}> {
-  props: P;
-
-  constructor(props: P) {
-    this.props = props;
-  }
-
-  template(props: P) {
-    return props.children;
-  }
+export interface FunctionComponent<P = {}> {
+  (props: PropsWithChildren<P>): JSX.Element;
+  displayName?: string;
 }
 
-export function createComponent<T>(Comp: Component<T> | FunctionComponent<T>, props: T): JSX.Element {
-  const staticProps = Object.freeze(props);
+export type ComponentType<P = {}> = FunctionComponent<P>;
 
-  if ((Comp as any).prototype instanceof Component) {
-    return untracked(() => {
-      const comp: Component<T> = new (Comp as any)(staticProps);
-      return comp.template(staticProps);
-    });
-  }
-  
-  return untracked(() => (Comp as FunctionComponent<T>)(staticProps));
+export function createComponent<T>(Comp: FunctionComponent<T>, props: T): JSX.Element {
+  const staticProps = Object.freeze(props);
+  return untracked(() => Comp(staticProps));
 }
