a3490deb694e13957e21f4623ddef3d580e8554b
Maciej Kwaśniak <kmaciek17@gmail.com>
2020-09-20 14:53:23 +0200

    Just component

--- packages/@vzn/core/src/rendering/component.ts
+++ packages/@vzn/core/src/rendering/component.ts
@@ -2,22 +2,19 @@ import { untracked } from "mobx";
 
 export type PropsWithChildren<P> = P & { children?: JSX.Element };
 
-export type ComponentProps<T extends keyof JSX.IntrinsicElements | ComponentType<any>> =
-  T extends ComponentType<infer P>
+export interface Component<P = {}> {
+  (props: PropsWithChildren<P>): JSX.Element;
+  displayName?: string;
+}
+
+export type ComponentProps<T extends keyof JSX.IntrinsicElements | Component<any>> =
+  T extends Component<infer P>
     ? P
     : T extends keyof JSX.IntrinsicElements
         ? JSX.IntrinsicElements[T]
         : {};
 
-
-export interface FunctionComponent<P = {}> {
-  (props: PropsWithChildren<P>): JSX.Element;
-  displayName?: string;
-}
-
-export type ComponentType<P = {}> = FunctionComponent<P>;
-
-export function createComponent<T>(Comp: FunctionComponent<T>, props: T): JSX.Element {
+export function createComponent<P>(componentCreator: Component<P>, props: P): JSX.Element {
   const staticProps = Object.freeze(props);
-  return untracked(() => Comp(staticProps));
+  return untracked(() => componentCreator(staticProps));
 }
