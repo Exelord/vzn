95e770f3f923f678439a5ac1ba6fa63e2e3933e9
Maciej Kwaśniak <kmaciek17@gmail.com>
2021-03-31 23:29:02 +0200

    Add tests to memo

---
+++ packages/@vzn/reactivity/tests/memo.test.ts
@@ -0,0 +1,122 @@
+import { batch, createContainer, runWithContainer } from '../src/container';
+import { createMemo } from '../src/memo';
+import { createRenderEffect } from '../src/effect';
+import { createValue } from '../src/value';
+
+describe('createMemo', () => {
+  it('does not recompute if not changed', () => {
+    const spy = jest.fn();
+    
+    expect(spy.mock.calls.length).toBe(0);
+    
+    const getMemo = createMemo(() => {
+      spy();
+    });
+    
+    expect(spy.mock.calls.length).toBe(0);
+
+    getMemo();
+    
+    expect(spy.mock.calls.length).toBe(1);
+    
+    getMemo();
+    
+    expect(spy.mock.calls.length).toBe(1);
+  });
+  
+  it('does recompute if changed', () => {
+    const [getSignal, setSignal] = createValue(1);
+    const spy = jest.fn();
+    
+    expect(spy.mock.calls.length).toBe(0);
+    
+    const getMemo = createMemo(() => {
+      getSignal();
+      spy();
+    });
+    
+    setSignal(2);
+    
+    expect(spy.mock.calls.length).toBe(0);
+    
+    getMemo();
+    
+    expect(spy.mock.calls.length).toBe(1);
+    
+    setSignal(3);
+    setSignal(4);
+    
+    getMemo();
+    
+    expect(spy.mock.calls.length).toBe(2);
+  });
+  
+  it('does recompute inside batch only once', () => {
+    const [getSignal, setSignal] = createValue(1);
+    const spy = jest.fn();
+    
+    expect(spy.mock.calls.length).toBe(0);
+
+    batch(() => {
+      const getMemo = createMemo(() => {
+        getSignal();
+        spy();
+      });
+      
+      expect(spy.mock.calls.length).toBe(0);
+  
+      setSignal(2);
+      setSignal(3);
+      
+      expect(spy.mock.calls.length).toBe(0);
+      
+      getMemo();
+
+      expect(spy.mock.calls.length).toBe(1);
+    })
+
+    expect(spy.mock.calls.length).toBe(1);
+  });
+  
+  it('does recompute on change in effects', () => {
+    const [getSignal, setSignal] = createValue(1);
+    const container = createContainer(() => {});
+    const spy = jest.fn();
+    
+    expect(spy.mock.calls.length).toBe(0);
+
+    const getMemo = createMemo(() => {
+      getSignal();
+      spy();
+    });
+
+    expect(spy.mock.calls.length).toBe(0);
+    
+    runWithContainer(container, () => {
+      createRenderEffect(() => {
+        getMemo();
+      })
+    })
+    
+    expect(spy.mock.calls.length).toBe(1);
+
+    setSignal(2);
+    setSignal(3);
+    
+    expect(spy.mock.calls.length).toBe(3);
+    
+    getMemo();
+
+    expect(spy.mock.calls.length).toBe(3);
+    
+    container.dispose();
+    
+    setSignal(4);
+
+    expect(spy.mock.calls.length).toBe(3);
+    
+    getMemo();
+
+    expect(spy.mock.calls.length).toBe(4);
+  });
+});
