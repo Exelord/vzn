1510ed718414c31857f63f97eb2033dd8bd91a2c
Maciej Kwaśniak <kmaciek17@gmail.com>
2021-04-04 20:37:45 +0200

    Optimize

--- packages/@vzn/reactivity/src/container.ts
+++ packages/@vzn/reactivity/src/container.ts
@@ -13,8 +13,6 @@ export interface Container {
   addDisposer(fn: Disposer): void;
 }
 
-function dummyFn() {}
-
 let globalContainer: Container | undefined;
 
 export function getContainer(): Container | undefined {
@@ -57,7 +55,7 @@ export function cleanup(fn: Disposer) {
 }
 
 export function createContainer(
-  computation: Computation<void> = dummyFn,
+  computation?: Computation<void>,
   isPrioritized = false
 ): Container {
   let isPaused = false;
@@ -66,70 +64,78 @@ export function createContainer(
   const computationsQueue = new Set<Computation<void>>();
   const delayedQueue = new Set<Computation<void>>();
 
-  const container = Object.freeze({
+  function recompute() {
+    if (!computation) return;
+    
+    const parentContainer = getContainer();
+  
+    if (parentContainer && !isPrioritized) {
+      parentContainer.schedule(computation);
+    } else {
+      untrack(computation);
+    }
+  }
+
+  function schedule(fn: Computation<void>) {
+    if (isPaused) {
+      computationsQueue.add(fn);
+    } else {
+      untrack(fn);
+    }
+  }
+
+  function scheduleDelayed(fn: Computation<void>) {
+    if (isPaused) {
+      delayedQueue.add(fn);
+    } else {
+      untrack(fn);
+    }
+  }
+
+  function pause() {
+    isPaused = true;
+  }
+
+  function resume() {
+    if (!isPaused) return;
+
+    isPaused = false;
+    
+    const computations = [...computationsQueue];
+    const delayed = [...delayedQueue];
+    
+    computationsQueue.clear();
+    delayedQueue.clear();
+    
+    computations.forEach(async (fn) => untrack(fn));
+    delayed.forEach(async (fn) => untrack(fn));
+  }
+
+  function dispose() {
+    const tmpDisposers = [...disposers]
+    
+    disposers.clear();
+    
+    tmpDisposers.forEach(async (disposer) => untrack(disposer));
+  }
+
+  function addDisposer(fn: Disposer) {
+    disposers.add(fn)
+  }
+
+  return Object.freeze({
+    recompute,
+    schedule,
+    scheduleDelayed,
+    pause,
+    resume,
+    dispose,
+    addDisposer,
+
     get isPaused() {
       return isPaused;
-    },
-
-    recompute() {
-      const parentContainer = getContainer();
-  
-      if (parentContainer && !isPrioritized) {
-        parentContainer.schedule(computation);
-      } else {
-        untrack(computation);
-      }
-    },
-  
-    schedule(fn: Computation<void>) {
-      if (isPaused) {
-        computationsQueue.add(fn);
-      } else {
-        untrack(fn);
-      }
-    },
-  
-    scheduleDelayed(fn: Computation<void>) {
-      if (isPaused) {
-        delayedQueue.add(fn);
-      } else {
-        untrack(fn);
-      }
-    },
-  
-    pause() {
-      isPaused = true;
-    },
-  
-    resume() {
-      if (!isPaused) return;
-
-      isPaused = false;
-      
-      const computations = [...computationsQueue];
-      const effects = [...delayedQueue];
-      
-      computationsQueue.clear();
-      delayedQueue.clear();
-      
-      computations.forEach(async (computation) => untrack(computation));
-      effects.forEach(async (computation) => untrack(computation));
-    },
-  
-    dispose() {
-      const tmpDisposers = [...disposers]
-      
-      disposers.clear();
-      
-      tmpDisposers.forEach(async (disposer) => untrack(disposer));
-    },
-  
-    addDisposer(fn: Disposer) {
-      disposers.add(fn)
     }
   });
-
-  return container;
 }
 
 export function batch<T>(computation: Computation<T>): T {
