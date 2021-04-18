2cc3900e2f3ed3bf601cd7fd88c27f8a2a6c5432
Maciej Kwaśniak <kmaciek17@gmail.com>
2021-04-18 10:35:46 +0200

    Fix value

--- packages/@vzn/reactivity/src/value.ts
+++ packages/@vzn/reactivity/src/value.ts
@@ -11,7 +11,8 @@ export function createValue<T>(
   compare?: boolean | ((prev: T | undefined, next: T) => boolean),
 ): [() => T | undefined, (newValue: T) => void] {
   const computations = new Set<Computation>();
-  
+  let tempComputations = new Set<Computation>();
+
   let currentValue = value;
   
   compare ??= true;
@@ -21,7 +22,11 @@ export function createValue<T>(
 
     if (computation && !computations.has(computation)) {
       computations.add(computation);
-      onCleanup(() => computations.delete(computation));
+
+      onCleanup(() => {
+        tempComputations.delete(computation)
+        computations.delete(computation)
+      });
     }
 
     return currentValue;
@@ -30,9 +35,12 @@ export function createValue<T>(
   function setter(newValue: T): void {
     if (typeof compare === 'function' && compare(currentValue, newValue)) return;
     if (compare === true && currentValue === newValue) return;
-
+    
     currentValue = newValue;
-    [...computations].forEach((computation) => computation.recompute());
+
+    tempComputations = new Set<Computation>(computations);
+    tempComputations.forEach((computation) => computation.recompute());
+    tempComputations.clear();
   }
 
   return [getter, setter];
---
+++ packages/@vzn/reactivity/tests/value.test.ts
@@ -0,0 +1,43 @@
+import { createValue } from "../src/value";
+import { createComputation } from "../src/computation";
+import { runWith } from "../src/utils";
+import { createDisposer } from "../src/disposer";
+
+describe('createValue', () => {
+  it('triggers computation', () => {
+    const spy = jest.fn();
+    const [getSignal, setSignal] = createValue(false);
+    const computation = createComputation(spy);
+
+    runWith({ computation }, () => getSignal());
+
+    expect(spy.mock.calls.length).toBe(0);
+    expect(getSignal()).toBe(false);
+    
+    setSignal(true);
+    
+    expect(spy.mock.calls.length).toBe(1);
+    expect(getSignal()).toBe(true);
+  });
+  
+  it('removes subscriptions on cleanup', () => {
+    const spy = jest.fn();
+    const [getSignal, setSignal] = createValue(false);
+    const computation = createComputation(spy);
+    const disposer = createDisposer();
+
+    runWith({ disposer, computation }, () => getSignal());
+
+    setSignal(true);
+    
+    expect(spy.mock.calls.length).toBe(1);
+    expect(getSignal()).toBe(true);
+
+    disposer.flush();
+
+    setSignal(false);
+    
+    expect(spy.mock.calls.length).toBe(1);
+    expect(getSignal()).toBe(false);
+  });
+});
