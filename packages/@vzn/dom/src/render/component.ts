8686531dc6bb854403f1deb3c6a98fcc068f8624
Maciej Kwaśniak <kmaciek17@gmail.com>
2021-04-24 17:18:49 +0200

    Batch components

--- packages/@vzn/dom/src/render/component.ts
+++ packages/@vzn/dom/src/render/component.ts
@@ -1,6 +1,6 @@
-import { untrack } from "../reactivity";
+import { batch, untrack } from "../reactivity";
 import { JSX } from "../jsx";
 
 export function component<T>(Comp: (props: T) => JSX.Element, props: T): JSX.Element {
-  return untrack(() => Comp(props as T));
+  return untrack(() => batch(() => Comp(props as T)));
 }
