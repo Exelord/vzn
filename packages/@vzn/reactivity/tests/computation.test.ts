3781b0c72cca4fe139598c08ccf9128de6130273
Maciej Kwaśniak <kmaciek17@gmail.com>
2021-05-01 10:38:50 +0200

    Move untrack tests to correct place

--- packages/@vzn/reactivity/tests/computation.test.ts
+++ packages/@vzn/reactivity/tests/computation.test.ts
@@ -1,5 +1,8 @@
 import { batch } from '../src/batch';
-import { createComputation } from '../src/computation';
+import { createComputation, untrack } from '../src/computation';
+import { onCleanup } from '../src/disposer';
+import { getOwner, runWithOwner } from '../src/owner';
+import { createQueue } from '../src/queue';
 
 jest.useFakeTimers('modern');
 
@@ -56,4 +59,42 @@ describe('createComputation', () => {
     expect(spy.mock.calls.length).toBe(1);
   });
 });
-  
+
+describe('untrack', () => {
+  it('runs without any computation', () => {
+    const computation = createComputation(() => {});
+    
+    expect(getOwner().computation).toBeUndefined();
+    
+    runWithOwner({ computation }, () => {
+      expect(getOwner().computation).toBe(computation);
+      
+      untrack(() => {
+        expect(getOwner().computation).toBeUndefined();
+      });
+
+      expect(getOwner().computation).toBe(computation);
+    });
+    
+    expect(getOwner().computation).toBeUndefined();
+  });
+
+  it('runs cleanups in computation correctly', () => {
+    const disposer = createQueue();
+    const cleanupMock = jest.fn();
+    
+    expect(getOwner().disposer).toBeUndefined();
+    
+    runWithOwner({ disposer }, () => {
+      untrack(() => {
+        onCleanup(cleanupMock)
+      });
+    });
+    
+    expect(cleanupMock.mock.calls.length).toBe(0);
+
+    disposer.flush();
+    
+    expect(cleanupMock.mock.calls.length).toBe(1);
+  });
+});
