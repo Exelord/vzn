5af0d43add5cbaed1cc7c43104d57d71cd8fd7cd
Maciej Kwaśniak <kmaciek17@gmail.com>
2021-05-01 11:27:34 +0200

    Add createRoot tests

--- packages/@vzn/reactivity/src/root.ts
+++ packages/@vzn/reactivity/src/root.ts
@@ -1,6 +1,14 @@
 import { runWithOwner } from "./owner";
 import { createQueue } from "./queue";
 
+/**
+ * Computations created by root live until dispose is called
+ * 
+ * @export
+ * @template T
+ * @param {(disposer: () => void) => T} fn
+ * @returns {T}
+ */
 export function createRoot<T>(fn: (disposer: () => void) => T): T {
   const disposer = createQueue();
   return runWithOwner({ disposer, computation: undefined }, () => fn(disposer.flush));
---
+++ packages/@vzn/reactivity/tests/root.test.ts
@@ -0,0 +1,89 @@
+import { createRoot } from "../src/root";
+import { createValue } from "../src/value";
+import { createInstantEffect } from "../src/effect";
+
+describe('createRoot', () => {
+  it("allows subcomputations to escape their parents", () => {
+    createRoot(() => {
+      const [getOuterSignal, setOuterSignal] = createValue(0);
+      const [getInnerSignal, setInnerSignal] = createValue(0);
+      const outerSpy = jest.fn();
+      const innerSpy = jest.fn();
+      
+      createInstantEffect(() => {
+        getOuterSignal();
+        outerSpy();
+
+        createRoot(() => {
+          createInstantEffect(() => {
+            getInnerSignal();
+            innerSpy();
+          });
+        });
+      });
+      
+      expect(outerSpy.mock.calls.length).toBe(1);
+      expect(innerSpy.mock.calls.length).toBe(1);
+      
+      // trigger the outer computation, making more inners
+      setOuterSignal(1);
+      setOuterSignal(2);
+      
+      expect(outerSpy.mock.calls.length).toBe(3);
+      expect(innerSpy.mock.calls.length).toBe(3);
+      
+      setInnerSignal(1);
+      
+      expect(outerSpy.mock.calls.length).toBe(3);
+      expect(innerSpy.mock.calls.length).toBe(6);
+    });
+  });
+  
+  it("does not batch updates when used at top level", () => {
+    const spy = jest.fn();
+
+    createRoot(() => {
+      const [getSignal, setSignal] = createValue(1);
+      
+      createInstantEffect(() => {
+        getSignal();
+        spy();
+      });
+      
+      expect(spy.mock.calls.length).toBe(1);
+      
+      setSignal(2);
+      
+      expect(spy.mock.calls.length).toBe(2);
+      
+      setSignal(3);
+      
+      expect(spy.mock.calls.length).toBe(3);
+    });
+  });
+  
+  it("allows to dispose all nested computations", () => {
+    const spy = jest.fn();
+
+    createRoot((dispose) => {
+      const [getSignal, setSignal] = createValue(1);
+      
+      createInstantEffect(() => {
+        getSignal();
+        spy();
+      });
+      
+      expect(spy.mock.calls.length).toBe(1);
+      
+      setSignal(2);
+      
+      expect(spy.mock.calls.length).toBe(2);
+
+      dispose();
+      
+      setSignal(3);
+      
+      expect(spy.mock.calls.length).toBe(2);
+    });
+  });
+});
