9ed94c8bb8e31c75fb9a420e1b181864fd44378d
Maciej Kwaśniak <kmaciek17@gmail.com>
2021-07-13 22:18:34 +0200

    Update reactivity

--- packages/@vzn/rendering/src/core.ts
+++ packages/@vzn/rendering/src/core.ts
@@ -1,5 +1,5 @@
 export { createComponent } from "./render/component";
-export { createMemo as memo, react as effect, root } from "@vzn/reactivity";
+export { createMemo as memo, reactive as effect, root } from "@vzn/reactivity";
 
 export const getOwner = null;
 export const sharedConfig = {};
