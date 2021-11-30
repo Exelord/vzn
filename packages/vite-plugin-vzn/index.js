b084b075f676a1d6b6d898736c0f0e13359cdabe
Maciej Kwaśniak <kmaciek17@gmail.com>
2021-11-30 09:58:05 +0100

    Improve vite plugin

--- packages/vite-plugin-vzn/index.js
+++ packages/vite-plugin-vzn/index.js
@@ -2,16 +2,55 @@ const { transformAsync } = require("@babel/core");
 const babelPresetVzn = require("babel-preset-vzn");
 const babelPresetTypescript = require("@babel/preset-typescript");
 
+function normalizeAliases(alias) {
+  return Array.isArray(alias)
+    ? alias
+    : Object.entries(alias).map(([find, replacement]) => ({
+        find,
+        replacement,
+      }));
+}
+
 module.exports = function Plugin() {
+  let projectRoot = process.cwd();
+
   return {
     name: "vzn",
     enforce: "pre",
+
+    config(userConfig, te) {
+      projectRoot = userConfig.root;
+
+      // TODO: remove when fully removed from vite
+      const legacyAlias = normalizeAliases(userConfig.alias || []);
+
+      if (!userConfig.resolve) userConfig.resolve = {};
+      userConfig.resolve.alias = [
+        ...legacyAlias,
+        ...normalizeAliases(userConfig.resolve?.alias || []),
+      ];
+
+      const nestedDeps = [];
+
+      return {
+        esbuild: { include: /\.[jt]s$/ },
+        optimizeDeps: {
+          include: nestedDeps,
+        },
+      };
+    },
 
     async transform(source, id) {
       if (!/\.[jt]sx/.test(id)) return null;
 
       const opts = {
+        babelrc: false,
+        configFile: false,
+        root: projectRoot,
         filename: id,
+        sourceFileName: id,
+        sourceMaps: true,
+        inputSourceMap: false,
         presets: [babelPresetVzn],
       };
 
